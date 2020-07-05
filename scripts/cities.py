dfeaab019d721922393f0682cb778123391e8b23
Marek Bejda <marek.bejda@ibm.com>
2020-07-05 08:58:11 -0400

    dataset ready

--- scripts/cities.py
+++ scripts/cities.py
@@ -14,15 +14,16 @@ if not exists("scripts/tmp"):
     mkdir("scripts/tmp")
 
 source = "https://download.geonames.org/export/dump/"
-dl = "http://download.geonames.org/export/dump/cities15000.zip"
+dl = "http://download.geonames.org/export/dump/%s.zip"
 cache = join("scripts/tmp", "main.html")
 
 names = "geonameid,name,asciiname,alternatenames,latitude,longitude,feature class,feature code,country code,cc2,admin1 code,admin2 code,admin3 code,admin4 code,population,elevation,dem,timezone,modification date".split(",")
 def retrieve():
-    z = request.urlopen(dl)
-    myzip = ZipFile(BytesIO(z.read())).extract('cities15000.txt')
+    name = "cities500"
+    z = request.urlopen(dl % name)
+    myzip = ZipFile(BytesIO(z.read())).extract('%s.txt' % name)
     d = pd.read_csv(myzip, delimiter="\t", names=names)
-    d.to_csv("data/cities15000.csv", index=False)
+    d.to_csv("data/%s.csv" % name, index=False)
 
 
 def process():
